e6ccdb27da5ee842ccf15622b37f4b232adf1e69
Konstantin Lyubort <lyubortk@gmail.com>
2019-09-12 02:13:15 +0300

    initial commit

---
+++ src/main/java/ru/hse/lyubortk/jdeodoranttesttask/CreateASTAction.java
@@ -0,0 +1,179 @@
+package ru.hse.lyubortk.jdeodoranttesttask;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParseStart;
+import com.github.javaparser.Providers;
+import com.github.javaparser.ast.Node;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowAnchor;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
+import com.intellij.ui.treeStructure.Tree;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Menu action which constructs an abstract syntax tree from a selection of characters.
+ */
+public class CreateASTAction extends AnAction {
+    static final String NOTIFICATION_GROUP_DISPLAY_ID = "JDeodorant test task";
+
+    private static final String NOTIFICATION_TITLE = "AST building error";
+    private static final String NOTIFICATION_CONTENT = "Could not create AST from selected code";
+    private static final String TOOL_WINDOW_ID = "Abstract Syntax Tree";
+    private static final List<ParseStart<? extends Node>> PARSE_OPTIONS = Arrays.asList(
+            ParseStart.COMPILATION_UNIT,
+            ParseStart.BLOCK,
+            ParseStart.CLASS_BODY,
+            ParseStart.IMPORT_DECLARATION,
+            ParseStart.MODULE_DIRECTIVE,
+            ParseStart.ANNOTATION,
+            ParseStart.TYPE,
+            ParseStart.PACKAGE_DECLARATION
+    );
+
+    /*
+     * Registers PasteActionHandlerWithNotifications handler.
+     * Action classes are guaranteed to be loaded into jvm and therefore their static initialization
+     * section could be used to register handlers.
+     */
+    static {
+        PasteActionHandlerWithNotifications.registerHandler();
+    }
+
+    /**
+     * Constructs abstract syntax tree from text selected by the primary caret.
+     * @param e  Event related to this action
+     */
+    @Override
+    public void actionPerformed(@NotNull final AnActionEvent e) {
+        final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = e.getRequiredData(CommonDataKeys.PROJECT);
+        final Document document = editor.getDocument();
+        final Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+
+        final Node astRootNode = parseJavaCode(document, primaryCaret);
+        if (astRootNode == null) {
+            sendNotification();
+            return;
+        }
+
+        showAST(project, astRootNode);
+    }
+
+    /**
+     * Sets visibility and enables menu item if all conditions are met:
+     *   A project is open,
+     *   An editor is active,
+     *   Some characters are selected
+     * @param e  Event related to this action
+     */
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        final Project project = e.getProject();
+        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        e.getPresentation().setEnabled(
+                project != null
+                        && editor != null
+                        && editor.getSelectionModel().hasSelection()
+        );
+    }
+
+    @Nullable
+    private Node parseJavaCode(Document document, Caret primaryCaret) {
+        final int start = primaryCaret.getSelectionStart();
+        final int end = primaryCaret.getSelectionEnd();
+        final String selectedText = document.getText(new TextRange(start, end));
+
+        Node astRootNode = null;
+        final JavaParser parser = new JavaParser();
+        for (ParseStart<? extends Node> parseStart : PARSE_OPTIONS) {
+            final String textToParse = parseStart.equals(ParseStart.BLOCK)
+                    ? "{" + selectedText + "}"
+                    : selectedText;
+
+            ParseResult<? extends Node> result =
+                    parser.parse(parseStart, Providers.provider(textToParse));
+
+            if (result.isSuccessful()) {
+                astRootNode = result.getResult().get(); // result is always present in this case
+                break;
+            }
+        }
+
+        // handle multiple CLASS_BODY elements (multiple methods or multiple fields)
+        if (astRootNode == null) {
+            ParseResult<? extends Node> result = parser.parse(
+                    ParseStart.CLASS_BODY,
+                    Providers.provider("class FakeClass {\n" + selectedText + "\n}")
+            );
+            astRootNode = result.getResult().orElse(null);
+        }
+        return astRootNode;
+    }
+
+    private void showAST(Project project, Node astRootNode) {
+        final Tree tree = new Tree(constructTreeNode(astRootNode));
+        final ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+
+        ToolWindow window = toolWindowManager.getToolWindow(TOOL_WINDOW_ID);
+        if (window == null) {
+            window = toolWindowManager.registerToolWindow(
+                    TOOL_WINDOW_ID,
+                    true,
+                    ToolWindowAnchor.RIGHT
+            );
+        }
+
+
+        final ContentManager windowContentManager = window.getContentManager();
+        final LocalDateTime now = LocalDateTime.now();
+        final String tabId = String.format(
+                "%02d:%02d:%02d", now.getHour(), now.getMinute(), now.getSecond()
+        );
+        final Content content = windowContentManager
+                .getFactory()
+                .createContent(new JBScrollPane(tree), tabId, false);
+        windowContentManager.addContent(content);
+    }
+
+    private DefaultMutableTreeNode constructTreeNode(@NotNull Node parseNode) {
+        final DefaultMutableTreeNode root = new DefaultMutableTreeNode(getNodeMessage(parseNode));
+        for (Node child : parseNode.getChildNodes()) {
+            root.add(constructTreeNode(child));
+        }
+        return root;
+    }
+
+    private String getNodeMessage(@NotNull Node parseNode) {
+        return parseNode.getClass().getSimpleName() + ":      '" + parseNode.toString() + "'";
+    }
+
+    static private void sendNotification() {
+        Notifications.Bus.notify(new Notification(
+                NOTIFICATION_GROUP_DISPLAY_ID,
+                NOTIFICATION_TITLE,
+                NOTIFICATION_CONTENT,
+                NotificationType.ERROR
+        ));
+    }
+}
---
+++ src/main/java/ru/hse/lyubortk/jdeodoranttesttask/PasteActionHandlerWithNotifications.java
@@ -0,0 +1,79 @@
+package ru.hse.lyubortk.jdeodoranttesttask;
+
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.editor.actionSystem.EditorActionManager;
+import com.intellij.openapi.editor.actionSystem.EditorTextInsertHandler;
+import com.intellij.util.Producer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.datatransfer.Transferable;
+
+import static ru.hse.lyubortk.jdeodoranttesttask.CreateASTAction.NOTIFICATION_GROUP_DISPLAY_ID;
+
+/**
+ * This class is an implementation of EditorActionHandler and EditorTextInsertHandler which
+ * creates notification when some text is pasted into the editor.
+ */
+public class PasteActionHandlerWithNotifications extends EditorActionHandler implements EditorTextInsertHandler {
+    private static final String NOTIFICATION_TITLE = "Paste action performed";
+    private static final String NOTIFICATION_CONTENT = "Запахам в коде скажем нет!";
+
+    private final EditorActionHandler originalHandler;
+
+    private PasteActionHandlerWithNotifications(@Nullable EditorActionHandler originalHandler) {
+        this.originalHandler = originalHandler;
+    }
+
+    /**
+     * Registers handler for ACTION_EDITOR_PASTE and ACTION_EDITOR_PASTE_SIMPLE.
+     */
+    public static void registerHandler() {
+        final EditorActionManager actionManager = EditorActionManager.getInstance();
+        registerHandler(actionManager, IdeActions.ACTION_EDITOR_PASTE);
+        registerHandler(actionManager, IdeActions.ACTION_EDITOR_PASTE_SIMPLE);
+    }
+
+    private static void registerHandler(@NotNull EditorActionManager actionManager,
+                                        @NotNull String ideAction) {
+        final EditorActionHandler originalHandler = actionManager.getActionHandler(ideAction);
+        actionManager.setActionHandler(
+                ideAction,
+                new PasteActionHandlerWithNotifications(originalHandler)
+        );
+    }
+
+    @Override
+    protected void doExecute(@NotNull Editor editor,
+                             @Nullable Caret caret,
+                             DataContext dataContext) {
+        if (originalHandler != null) {
+            originalHandler.execute(editor, caret, dataContext);
+        }
+        sendNotification();
+    }
+
+    @Override
+    public void execute(Editor editor, DataContext dataContext, Producer<Transferable> producer) {
+        if (originalHandler != null && originalHandler instanceof EditorTextInsertHandler) {
+            ((EditorTextInsertHandler)originalHandler).execute(editor, dataContext, producer);
+        }
+        sendNotification();
+    }
+
+    static private void sendNotification() {
+        Notifications.Bus.notify(new Notification(
+                NOTIFICATION_GROUP_DISPLAY_ID,
+                NOTIFICATION_TITLE,
+                NOTIFICATION_CONTENT,
+                NotificationType.INFORMATION
+        ));
+    }
+}
